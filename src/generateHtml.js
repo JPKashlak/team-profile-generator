3646a0d590243fbffd9278c6b08fa0f638daf73d
jpkashlak <jpkashlak@gmail.com>
2021-02-14 19:14:59 -0600

    managed to print all role's data onto page

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,10 +1,78 @@
-const generateHtml = data => {
-    console.log(data)
+
+const generateHtml = data => {  
+    // const { mana, engi, int } = data
     const dataPull = data
     const dataArray = Object.entries(dataPull)
-    console.log(dataArray[0][1].constructor.name)
+    console.log(dataArray.length)
 
-        
+    const managers = () => {
+        var i;
+        for(i = 0; i < dataArray.length; i++) {
+            if (dataArray[i][1].constructor.name === 'Manager') {
+                console.log(dataArray[i][1].name + " the " + dataArray[i][1].constructor.name)
+                
+                return `
+                <div class = "card mx-auto">
+                    <div class = "card-header bg-primary text-light text-center">
+                        <h5 class="manaName">${dataArray[0][1].constructor.name}: ${dataArray[0][1].name}</h5>
+                    </div>
+                    <div class = "card-body bg-light text-center">
+                        <h6>ID: ${dataArray[0][1].id}</h6>
+                        <h6>Email: <a href="mailto:${dataArray[0][1].email}">Click Here To Contact!</a></h6>
+                        <h6>Office Number: ${dataArray[0][1].officeNum}</h6>
+                    </div>
+                </div>
+            `
+            }
+        }
+    }
+    
+    const engineers = () => {
+        var i;
+        for(i = 0; i < dataArray.length; i++) { 
+            if (dataArray[i][1].constructor.name === 'Engineer') {
+            console.log(dataArray[i][1].name + " the " + dataArray[i][1].constructor.name)
+    
+           return `
+            <div class = "card mx-auto">
+                <div class = "card-header bg-primary text-light text-center">
+                    <h5 class="engiName">${dataArray[i][1].constructor.name}: ${dataArray[i][1].name}</h5>
+                </div>
+                <div class = "card-body bg-light text-center">
+                    <h6>ID: ${dataArray[i][1].id}</h6>
+                    <h6>Email: <a href="mailto:${dataArray[i][1].email}">Click Here To Contact!</a></h6>
+                    <h6>GitHub Username: ${dataArray[i][1].github}</h6>
+                </div>
+            </div>
+            `    
+            }
+        }
+    }
+    
+    const interns = () => {
+        var i;
+        for(i = 0; i < dataArray.length; i++) {
+            if (dataArray[i][1].constructor.name === 'Intern') {
+            console.log(dataArray[i][1].name + " the " + dataArray[i][1].constructor.name)
+    
+            return `
+            <div class = "card mx-auto">
+                <div class = "card-header bg-primary text-light text-center">
+                    <h5 class="engiName">${dataArray[i][1].constructor.name}: ${dataArray[i][1].name}</h5>
+                </div>
+                <div class = "card-body bg-light text-center">
+                    <h6>ID: ${dataArray[i][1].id}</h6>
+                    <h6>Email: <a href="mailto:${dataArray[i][1].email}">Click Here To Contact!</a></h6>
+                    <h6>GitHub School: ${dataArray[i][1].school}</h6>
+                </div>
+            </div>
+            `
+            }
+        }
+    }
+
+
+    
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -21,15 +89,10 @@ const generateHtml = data => {
     <h1 class = "text-light">${dataArray[0][1].name}'s Team</h1>
 </header>
 
-<div class = "card mx-auto">
-    <div class = "card-header bg-primary text-light text-center">
-        <h5 class="manaName">${dataArray[0][1].constructor.name}: ${dataArray[0][1].name}</h5>
-    </div>
-    <div class = "card-body bg-light text-center">
-        <h6>ID: ${dataArray[0][1].id}</h6>
-        <h6>Email: ${dataArray[0][1].email}</h6>
-        <h6>Office Number: ${dataArray[0][1].officeNum}</h6>
-    </div>
+<div>
+    ${managers(managers)}
+    ${engineers(engineers)}
+    ${interns(interns)}
 </div>
 
 
